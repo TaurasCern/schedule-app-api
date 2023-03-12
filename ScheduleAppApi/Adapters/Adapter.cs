324aa5b8eb6af2e0a2704279cde9a9a20fa9eff9
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-03-12 18:04:45 +0200

    Added ScheduledTime DTOs

--- ScheduleAppApi/Adapters/Adapter.cs
+++ ScheduleAppApi/Adapters/Adapter.cs
@@ -1,12 +1,33 @@
 ﻿using ScheduleAppApi.Adapters.IAdapters;
 using ScheduleAppApi.Domain.Models;
+using ScheduleAppApi.DTOs.ScheduledTime;
 using ScheduleAppApi.DTOs.User;
 
 namespace ScheduleAppApi.Adapters
 {
     public class Adapter : IAdapter
     {
-        public LoginResponse Bind(User user) => new() { Role = user.Role.ToString() };
+        public LoginResponse Bind(User user, string token) 
+            => new() { Role = user.Role.ToString(), Token = token };
+        public ScheduledTimeResponse[] Bind(List<ScheduledTime> scheduledTimes) 
+            => scheduledTimes == null ? Array.Empty<ScheduledTimeResponse>() :
+            scheduledTimes.Select(Bind).ToArray();
+        public ScheduledTimeResponse Bind(ScheduledTime st)
+            => new() 
+            {
+                Id = st.Id,
+                From = st.From,
+                To = st.To,
+                Note = st.Note,
+            };
 
+        public ScheduledTime Bind(ScheduledTimeRequest request, int userId)
+            => new()
+            {
+                From = request.From,
+                To = request.To,
+                Note = request.Note,
+                UserId = userId
+            };
     }
 }
--- ScheduleAppApi/Adapters/IAdapters/IAdapter.cs
+++ ScheduleAppApi/Adapters/IAdapters/IAdapter.cs
@@ -1,10 +1,14 @@
 ﻿using ScheduleAppApi.Domain.Models;
+using ScheduleAppApi.DTOs.ScheduledTime;
 using ScheduleAppApi.DTOs.User;
 
 namespace ScheduleAppApi.Adapters.IAdapters
 {
     public interface IAdapter
     {
-        LoginResponse Bind(User user);
+        LoginResponse Bind(User user, string token);
+        ScheduledTimeResponse[] Bind(List<ScheduledTime> scheduledTimes);
+        ScheduledTimeResponse Bind(ScheduledTime scheduledTime);
+        ScheduledTime Bind(ScheduledTimeRequest request, int userId);
     }
 }
