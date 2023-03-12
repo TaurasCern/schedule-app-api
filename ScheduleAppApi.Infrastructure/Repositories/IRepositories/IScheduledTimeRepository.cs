1e222fb134f61c4b9a5c88fd305b5d17303a0473
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-03-12 18:05:19 +0200

    created ScheduledTime repository

---
+++ ScheduleAppApi.Infrastructure/Repositories/IRepositories/IScheduledTimeRepository.cs
@@ -0,0 +1,14 @@
+﻿using ScheduleAppApi.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleAppApi.Infrastructure.Repositories.IRepositories
+{
+    public interface IScheduledTimeRepository : IRepository<ScheduledTime>
+    {
+
+    }
+}
---
+++ ScheduleAppApi.Infrastructure/Repositories/ScheduledTimeRepository.cs
@@ -0,0 +1,16 @@
+﻿using ScheduleAppApi.Domain.Models;
+using ScheduleAppApi.Infrastructure.Database;
+using ScheduleAppApi.Infrastructure.Repositories.IRepositories;
+
+namespace ScheduleAppApi.Infrastructure.Repositories
+{
+    public class ScheduledTimeRepository : Repository<ScheduledTime>, IScheduledTimeRepository
+    {
+        private readonly ScheduleContext _context;
+        public ScheduledTimeRepository(ScheduleContext context)
+            : base(context)
+        {
+            _context = context;
+        }
+    }
+}
